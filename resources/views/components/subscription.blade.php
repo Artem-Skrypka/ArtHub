259724481fe356cba84116085545587211c6b155
Artem-Skrypka <artemskripka2001@gmail.com>
2023-07-12 13:53:52 +0000

    added profile and subscription

---
+++ resources/views/components/subscription.blade.php
@@ -0,0 +1,8 @@
+<div class="flex flex-col p-2 border-b border-pink-800 mb-1 last:mb-0 last:border-0">
+    <h3 class=" whitespace-pre-line">Subscription title</h3>
+    <p class=" text-pink-300"><span class=" text-emerald-300">$</span> 1 per month</p>
+    <p class=" ">
+        early acces for my content, and full content.
+    </p>
+    <button class="border border-pink-800 rounded-md min-[320px]:min-w-[90px] md:min-w-[200px] p-1 my-1">Subscribe</button>
+</div>
