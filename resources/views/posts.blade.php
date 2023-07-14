693d6fbbc2d721d67a8f6814dc2540f73474d4be
Artem-Skrypka <artemskripka2001@gmail.com>
2023-07-14 16:47:21 +0000

    added burder icon

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -17,12 +17,12 @@
     <body class="antialiased pt-[70px]">
         <header class="fixed z-30 bg-white flex top-0 border-b border-pink-800 h-[70px] w-full">
             <div class="container mx-auto">
-                <div class="grid grid-cols-3 lg:grid-cols-[1fr_3fr_1fr] grid-rows-1 h-full">
+                <div class="grid grid-cols-[2fr_3fr_1fr] min-[1px]:max-sm:grid-cols-3 lg:grid-cols-[1fr_3fr_1fr] grid-rows-1 h-full">
                     <div class="flex h-full items-center w-full px-2">
 
                     </div>
 
-                    <div class="flex h-full items-center w-full px-2">
+                    <div class="flex h-full min-[1px]:max-sm:hidden items-center w-full px-2">
                         <ul class="flex w-full justify-center">
                             <li class=" mx-1 first:ml-0 last:mr-0"><a class=" text-pink-800 hover:text-pink-800 active:text-pink-800" href="">Home</a></li>
                             <li class=" mx-1 first:ml-0 last:mr-0 text-darkblue"><a href="" class="hover:text-pink-800 active:text-pink-800">Creators</a></li>
@@ -32,7 +32,18 @@
                         </ul>
                     </div>
 
-                    <div class="flex relative z-30 h-full items-center justify-end lg:justify-center w-full px-2">
+                    <div class="min-[1px]:max-sm:flex sm:hidden h-full items-center justify-center   w-full px-2">
+                        <div class="burger-menu min-[1px]:max-sm:flex sm:hidden flex-col justify-between cursor-pointer w-[20px] h-[13px]">
+                            {{-- <span class=" w-full h-[2px] bg-pink-800 before:flex before:content-[''] before:mt-[5px] before:h-[2px] before:w-full before:bg-pink-800 before:transition-all after:content-[''] after:mt-[3px] after:h-[2px] after:w-full after:bg-pink-800 after:flex after:transition-all transition-all"></span> --}}
+                            <span class=" w-full flex h-[2px] bg-pink-800 transition-all rounded-lg"></span>
+                            <span class=" w-full flex h-[2px] bg-pink-800 transition-all rounded-lg"></span>
+                            <span class=" w-full flex h-[2px] bg-pink-800 transition-all rounded-lg"></span>
+                        </div>
+                    </div>
+
+                    
+
+                    <div class="flex relative z-30 h-full items-center justify-end min-[1px]:max-sm:justify-center lg:justify-center w-full px-2">
                         <div class="flex sm:max-lg:relative w-max user-dropdown hover:cursor-pointer">
                             <span class="text-darkblue">iNSaNiTY</span>
                             <i class="fa-solid fa-chevron-up origin-center text-pink-800   transition-all user-arrow w-[15px] h-[15px] mt-[4px] ml-[4px]"></i>
