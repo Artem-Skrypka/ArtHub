c1da3d0e30908b77593f3ec035de65cf2ef78284
Artem-Skrypka <artemskripka2001@gmail.com>
2023-07-19 04:00:25 +0000

    complete post-card

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -7,11 +7,31 @@
         <time datetime="2023.02.18" class="text-slate-300">1 January 23:11</time>
     </div>
     <h2 class="px-1 font-semibold text-lg py-2 text-center">Anime AMW</h2>
-    <div class="video-block mx-1 mb-2 rounded-md">
+    <div class="video-block mx-1 my-1 rounded-md">
         <video controls autoplay muted>
             <source src="{{ asset('videos/Teeth.mp4') }}">
         </video>
         {{-- <iframe width="100%" class="2xl:h-[504px] xl:h-[420px] lg:h-[330px] md:h-[275px] sm:h-[230px]" src="https://www.youtube.com/embed/M-1FA3FiL9s" title="Teeth - AMV - 「 Anime MV」 - AnimeMix" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> --}}
     </div>
-    <p class="p-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident nesciunt voluptates laboriosam eligendi et unde repellendus fugit ex ut consectetur, pariatur quis libero excepturi voluptatibus quaerat expedita dolore explicabo facere iusto, earum soluta molestiae eveniet harum. Modi debitis eum provident illum laboriosam aspernatur, architecto placeat temporibus quibusdam natus repudiandae quaerat voluptatibus, eius perspiciatis quos voluptatum ut quas quod voluptas veritatis iste rerum quia? Molestias, cumque distinctio. Nobis sunt odit doloremque eligendi sed, nemo dolores ipsa magni totam laboriosam consequuntur dolor culpa quasi vel odio at molestiae quidem natus excepturi non a minima optio doloribus iure? Error obcaecati, sapiente cumque consectetur quibusdam atque nam ipsum temporibus sed neque nisi corrupti culpa rerum nesciunt sunt voluptas voluptatibus voluptatum adipisci quisquam ad illum a? Vero, asperiores atque qui explicabo provident recusandae minus ex nobis consectetur nisi maiores id sint illum reprehenderit ab fuga nam error officia consequatur. Ipsam impedit deserunt quisquam odit nihil?</p>
+    <div class="flex flex-col my-1 px-1">
+        <div class="">
+            <img class="post-image w-max h-max float-left mr-3 mt-2" src="{{ asset('images/chainsawman_bg.jpeg') }}" alt="">
+            <p class="p-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident nesciunt voluptates laboriosam eligendi et unde repellendus fugit ex ut consectetur, pariatur quis libero excepturi voluptatibus quaerat expedita dolore explicabo facere iusto, earum soluta molestiae eveniet harum. Modi debitis eum provident illum laboriosam aspernatur, architecto placeat temporibus quibusdam natus repudiandae quaerat voluptatibus, eius perspiciatis quos voluptatum ut quas quod voluptas veritatis iste rerum quia? Molestias, cumque distinctio. Nobis sunt odit doloremque eligendi sed, nemo dolores ipsa magni totam laboriosam consequuntur dolor culpa quasi vel odio at molestiae quidem natus excepturi non a minima optio doloribus iure? Error obcaecati, sapiente cumque consectetur quibusdam atque nam ipsum temporibus sed neque nisi corrupti culpa rerum nesciunt sunt voluptas voluptatibus voluptatum adipisci quisquam ad illum a? Vero, asperiores atque qui explicabo provident recusandae minus ex nobis consectetur nisi maiores id sint illum reprehenderit ab fuga nam error officia consequatur. Ipsam impedit deserunt quisquam odit nihil?</p>   
+        </div>
+    </div>
+    <div class="flex justify-between items-center border-t border-pink-800 p-1">
+        <div class="flex items-center">
+            <a class="flex items-center mr-2" href="#">
+                <i class="fa-solid text-pink-800  fa-comments mr-1 fa-lg"></i>
+                <p class="pr-1 text-pink-800">10</p>
+            </a>
+            <a class="flex items-center mr-2" href="#">
+                <i class="fa-solid text-pink-800 fa-eye mr-1"></i>
+                <p class="pr-1 text-pink-800">253</p>
+            </a>
+        </div>
+        <a href="#">
+            <i class="fa-solid text-pink-800 fa-share-nodes fa-lg"></i>
+        </a>
+    </div>
 </div>
